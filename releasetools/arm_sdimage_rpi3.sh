c30ae4cf696104a6a8fea5dec481233e432b9aba
Jean-Baptiste Boric <jblbeurope@gmail.com>
2016-05-15 17:54:17 +0200

    Initial creation of RPi image with U-Boot

---
+++ releasetools/arm_sdimage_rpi3.sh
@@ -0,0 +1,109 @@
+#!/usr/bin/env bash
+set -e
+
+#
+# This script creates a bootable image and should at some point in the future
+# be replaced by the proper NetBSD infrastructure.
+#
+
+#
+# Source settings if present
+#
+: ${SETTINGS_MINIX=.settings}
+if [ -f "${SETTINGS_MINIX}"  ]
+then
+	echo "Sourcing settings from ${SETTINGS_MINIX}"
+	# Display the content (so we can check in the build logs
+	# what the settings contain.
+	cat ${SETTINGS_MINIX} | sed "s,^,CONTENT ,g"
+	. ${SETTINGS_MINIX}
+fi
+
+: ${ARCH=evbearm-el}
+: ${TOOLCHAIN_TRIPLET=arm-elf32-minix-}
+: ${BUILDSH=build.sh}
+
+: ${SETS="minix-base"}
+: ${IMG=minix_arm_sd_rpi2.img}
+
+# ARM definitions:
+#: ${BUILDVARS=-V MKGCCCMDS=yes -V MKLLVM=no}
+# These BUILDVARS are for building with LLVM:
+: ${BUILDVARS=-V MKLIBCXX=no -V MKKYUA=no -V MKATF=no -V MKLLVMCMDS=no -V NOCLANGERROR=yes}
+
+if [ ! -f ${BUILDSH} ]
+then
+	echo "Please invoke me from the root source dir, where ${BUILDSH} is."
+	exit 1
+fi
+
+# we create a disk image of about 2 gig's
+# for alignment reasons, prefer sizes which are multiples of 4096 bytes
+: ${FAT_START=4096}
+: ${FAT_SIZE=$((    64*(2**20) - ${FAT_START} ))}
+: ${ROOT_SIZE=$((   64*(2**20) ))}
+: ${HOME_SIZE=$((  128*(2**20) ))}
+: ${USR_SIZE=$((  1792*(2**20) ))}
+#: ${IMG_SIZE=$((     2*(2**30) ))} # no need to build an image that big for now
+: ${IMG_SIZE=$((    64*(2**20) ))}
+
+# set up disk creation environment
+. releasetools/image.defaults
+. releasetools/image.functions
+
+${RELEASETOOLSDIR}/checkout_repo.sh -o ${RELEASETOOLSDIR}/u-boot -b ${UBOOT_BRANCH} -n ${UBOOT_REVISION} ${UBOOT_URL}
+${RELEASETOOLSDIR}/checkout_repo.sh -o ${RELEASETOOLSDIR}/rpi-firmware -b ${RPI_FIRMWARE_BRANCH} -n ${RPI_FIRMWARE_REVISION} ${RPI_FIRMWARE_URL}
+
+# where the kernel & boot modules will be
+MODDIR=${DESTDIR}/boot/minix/.temp
+
+echo "Building work directory..."
+build_workdir "$SETS"
+
+# IMG might be a block device
+if [ -f ${IMG} ]
+then
+	rm -f ${IMG}
+fi
+dd if=/dev/zero of=${IMG} bs=512 count=1 seek=$((($IMG_SIZE / 512) -1)) 2>/dev/null
+
+#
+# Generate /root, /usr and /home partition images.
+#
+echo "Writing disk image..."
+
+#
+# Write FAT bootloader partition
+#
+echo " * BOOT"
+rm -rf ${ROOT_DIR}/*
+cp -r releasetools/rpi-firmware/* ${ROOT_DIR}
+cp releasetools/u-boot/build/rpi/u-boot.bin ${ROOT_DIR}/u-boot-rpi.bin
+cp releasetools/u-boot/build/rpi_2/u-boot.bin ${ROOT_DIR}/u-boot-rpi2.bin
+cp releasetools/u-boot/build/rpi_3/u-boot.bin ${ROOT_DIR}/u-boot-rpi3.bin
+
+cat <<EOF >${ROOT_DIR}/config.txt
+[pi3]
+kernel=u-boot-rpi3.bin
+enable_uart=1
+
+[pi2]
+kernel=u-boot-rpi2.bin
+
+[pi1]
+kernel=u-boot-rpi.bin
+EOF
+
+${CROSS_TOOLS}/nbmakefs -t msdos -s $FAT_SIZE -O $FAT_START -o "F=32,c=1" ${IMG} ${ROOT_DIR} >/dev/null
+
+#
+# Write the partition table using the natively compiled
+# minix partition utility
+#
+${CROSS_TOOLS}/nbpartition -f -m ${IMG} $((${FAT_START}/512)) "c:$((${FAT_SIZE}/512))*"
+
+echo ""
+echo "Disk image at `pwd`/${IMG}"
+echo ""
+echo "To boot this image on kvm:"
+echo "qemu-system-arm -M raspi2 -drive if=sd,cache=writeback,format=raw,file=`pwd`/${IMG}"
