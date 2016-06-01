835c48137580f95fab6711a0c4fbb630dfb03144
Jean-Baptiste Boric <jblbeurope@gmail.com>
2016-06-01 10:09:45 +0200

    Presentation

---
+++ minix/drivers/storage/ramdisk/presentation.sh
@@ -0,0 +1,17 @@
+IMG_CUR=0
+IMG_MIN=1
+IMG_MAX=10
+
+while true
+do
+	if [ "$(cat /gpio/SLIDE_PREV)" = "0" ] && [ $IMG_CUR -gt $IMG_MIN ]
+	then
+		IMG_CUR=$((IMG_CUR - 1))
+		readpng /etc/presentation-$IMG_CUR.png > /dev/fb0
+	fi
+	if [ "$(cat /gpio/SLIDE_NEXT)" = "0" ] && [ $IMG_CUR -lt $IMG_MAX ]
+	then
+		IMG_CUR=$((IMG_CUR + 1))
+		readpng /etc/presentation-$IMG_CUR.png > /dev/fb0
+	fi
+done
